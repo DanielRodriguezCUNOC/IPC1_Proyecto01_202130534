3a0c9340bb1e46857b1dc5582ca5a9de6a258689
DanielRodriguezCUNOC <pabloalvarado202130534@cunoc.edu.gt>
2024-12-12 11:31:40 -0600

    Creacion del proyecto

---
+++ Cobra Kai Grafico/src/main/java/com/cobra/App.java
@@ -0,0 +1,12 @@
+package com.cobra;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Probando el repo");
+
+    }
+}
